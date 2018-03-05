0641f14395069a409684b72e55de76514ffe716c
Max Godfrey <34620214+MG2018@users.noreply.github.com>
2018-03-05 11:38:37 +0800

    Update christmas_cake.cpp

--- christmas_cake.cpp
+++ christmas_cake.cpp
@@ -6,12 +6,9 @@ int main()
     bool primes[100000];
     for (int i = 0; i < 100000; i++)
         primes[i] = 1;
-    for (int i = 2; i < 317; i++)
-    {
-        if (primes[i-1])
-        {
-            for (int j = 2; j*i <= 100000; j++)
-            {
+    for (int i = 2; i < 317; i++) {
+        if (primes[i-1]) {
+            for (int j = 2; j*i <= 100000; j++) {
                 primes[j*i-1] = 0;
             }
         }
@@ -19,8 +16,7 @@ int main()
     primes[0] = 0;
     int l, out = 0, f;
     scanf("%d", &l);
-    for (; l > 0; l--)
-    {
+    for (; l > 0; l--) {
         scanf("%d", &f);
         if (primes[f-1])
             ++out;
